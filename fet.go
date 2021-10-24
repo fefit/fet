6df32d390a4c1063000c1cc62bfa323e2293f64a
fefit <jxz_211@163.com>
2021-10-24 10:50:51 +0800

    lint: remove unnessary assignment

--- fet.go
+++ fet.go
@@ -425,7 +425,7 @@ func (node *Node) Compile(options *CompileOptions) (result string, err error) {
 			if contains(currentScopes, name) {
 				symbol = " = "
 			}
-			if compiledText, noDelimit, err = gen.Build(ast, genOptions, parseOptions); err != nil {
+			if compiledText, _, err = gen.Build(ast, genOptions, parseOptions); err != nil {
 				return "", node.halt("compile error:%s", err.Error())
 			}
 			result = delimit(addVarPrefix + name + localNS + symbol + compiledText)
@@ -479,7 +479,7 @@ func (node *Node) Compile(options *CompileOptions) (result string, err error) {
 					if key != defField {
 						value := prop.Raw
 						if ast, expErr := exp.Parse(value); expErr == nil {
-							if compiledText, noDelimit, err = gen.Build(ast, genOptions, parseOptions); err != nil {
+							if compiledText, _, err = gen.Build(ast, genOptions, parseOptions); err != nil {
 								return "", node.halt("compile error:%s", err.Error())
 							}
 							incLocalScopes = append(incLocalScopes, "$"+key)
@@ -508,7 +508,7 @@ func (node *Node) Compile(options *CompileOptions) (result string, err error) {
 				if expErr != nil {
 					return "", toError(expErr)
 				}
-				compiledText, noDelimit, err = gen.Build(ast, genOptions, parseOptions)
+				compiledText, _, err = gen.Build(ast, genOptions, parseOptions)
 				if err != nil {
 					return "", node.halt("parse 'foreach' error:%s", err.Error())
 				}
@@ -531,7 +531,7 @@ func (node *Node) Compile(options *CompileOptions) (result string, err error) {
 					if expErr != nil {
 						return "", toError(expErr)
 					}
-					compiledText, noDelimit, err = gen.Build(ast, genOptions, parseOptions)
+					compiledText, _, err = gen.Build(ast, genOptions, parseOptions)
 					if err != nil {
 						return "", node.halt("parse 'for' error:%s", err.Error())
 					}
@@ -549,7 +549,7 @@ func (node *Node) Compile(options *CompileOptions) (result string, err error) {
 				if expErr != nil {
 					return "", toError(expErr)
 				}
-				compiledText, noDelimit, err = gen.Build(ast, genOptions, parseOptions)
+				compiledText, _, err = gen.Build(ast, genOptions, parseOptions)
 				if err != nil {
 					return "", node.halt("parse 'for' statement error:%s", err.Error())
 				}
@@ -566,7 +566,7 @@ func (node *Node) Compile(options *CompileOptions) (result string, err error) {
 			if expErr != nil {
 				return "", toError(expErr)
 			}
-			compiledText, noDelimit, err = gen.Build(ast, genOptions, parseOptions)
+			compiledText, _, err = gen.Build(ast, genOptions, parseOptions)
 			if err != nil {
 				return "", node.halt("parse 'if' statement error:%s", err.Error())
 			}
@@ -595,7 +595,7 @@ func (node *Node) Compile(options *CompileOptions) (result string, err error) {
 			if expErr != nil {
 				return "", toError(expErr)
 			}
-			compiledText, noDelimit, err = gen.Build(ast, genOptions, parseOptions)
+			compiledText, _, err = gen.Build(ast, genOptions, parseOptions)
 			if err != nil {
 				return "", node.halt("parse 'if' statement error:%s", err.Error())
 			}
@@ -622,7 +622,7 @@ func (node *Node) Compile(options *CompileOptions) (result string, err error) {
 					if expErr != nil {
 						return "", toError(expErr)
 					}
-					compiledText, noDelimit, err = gen.Build(ast, genOptions, parseOptions)
+					compiledText, _, err = gen.Build(ast, genOptions, parseOptions)
 					if err != nil {
 						return "", node.halt("parse 'for' loop error:%s", err.Error())
 					}
@@ -631,7 +631,7 @@ func (node *Node) Compile(options *CompileOptions) (result string, err error) {
 						return "", toError(expErr)
 					}
 					var code string
-					if code, noDelimit, err = gen.Build(ast, genOptions, parseOptions); err != nil {
+					if code, _, err = gen.Build(ast, genOptions, parseOptions); err != nil {
 						return "", node.halt("parse 'for' loops error:%s", err.Error())
 					}
 					compiledText += " = " + code
