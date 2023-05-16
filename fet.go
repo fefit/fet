e327568bbf83f1c8c3d027c01f6e878944868aa1
fefit <jxz_211@163.com>
2023-05-16 21:58:56 +0800

    feat: add template types

--- fet.go
+++ fet.go
@@ -1,5 +1,9 @@
 package fet
 
+import (
+	"github.com/fefit/fet/lexer"
+)
+
 type Config struct {
 	LeftDelimiter  []byte
 	RightDelimiter []byte
@@ -13,11 +17,52 @@ func NewEngine() {
 
 }
 
-type BlockFeature struct {
-	MustLast bool
+type IParser interface {
+	// if the parser allowed clear
+	Clear() bool
 }
 
+type ICode interface {
+}
+
+type Property struct {
+	Shorthand      bool
+	Required       bool
+	Prop           string
+	ValueValidator func(token lexer.IToken) error
+}
+
+/**
+ * built-in property parser
+ */
+type BuiltInPropParser struct {
+	Props []Property
+}
+
+type BlockFeature struct {
+	Once   bool   // only allowed appear once
+	Last   bool   // only allowed appear at last feature
+	Name   []byte // feature block name
+	Alias  []byte // feature block alias name, if exists
+	Parser IParser
+}
+
+/**
+ *
+ */
 type Block struct {
 	Name     []byte
+	EndName  []byte
 	Features []BlockFeature
+	Parser   IParser
+	Childs   []ICode
+}
+
+type Context struct {
+	Scope []byte // scope
+
+}
+
+type OutputCode struct {
+	RawBytes [][]byte
 }
